fef3f1e19275920d3e51edd893424c9bce6d4687
r1zekami <110971527+r1zekami@users.noreply.github.com>
2025-03-24 13:08:00 +1000

    Update uiless.js

--- uiless.js
+++ uiless.js
@@ -3559,7 +3559,7 @@
                         initialSettings: {
                         AutoPlayVideo: true,
                         AutoConnect: true,
-                        ss: "ws://0.0.0.0:80",
+                        ss: "ws://176.99.130.225:80",
                         StartVideoMuted: true,
                         WaitForStreamer: true,
                         MinBitrate: 2000,
@@ -3567,6 +3567,14 @@
                         PreferredCodec: "H264",
                         MaxFPS: 60,
                         UseAdaptiveBitrate: true
+                        XRControllerInput: false,
+                        GamepadInput: false,
+                        TouchInput: false,
+                        MouseInput: false,
+                        KeyboardInput: false,
+                        bIgnoreKeyboardInput: true,
+                        bSuppressBrowserKeys: true,
+                        bUseArrayBuffer: true
                     }
                 }),
                 t = new Lt(e, {
